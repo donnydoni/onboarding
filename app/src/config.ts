47f2e4d06f3e61fd993443ff93fd22978e70fb0d
donnydoni <doniyornishonov@gmail.com>
2024-03-21 10:16:57 +0900

    onboarding

--- app/src/config.ts
+++ app/src/config.ts
@@ -14,55 +14,10 @@ export default defineConfig({
           pagination: 'count',
           title: 'Welcome to ellemments TOKYO!',
           description: "ellemments",
-          list: [
-            {
-              media: {
-                type: 'icon',
-                src: import('./assets/icons/guide.svg'),
-                size: 30,
-              },
-              text: 'Some cool feature',
-            },
-            {
-              media: {
-                type: 'icon',
-                src: import('./assets/icons/track.svg'),
-                size: 30,
-              },
-              text: 'Some very cool feature',
-            },
-            {
-              media: {
-                type: 'icon',
-                src: import('./assets/icons/time.svg'),
-                size: 30,
-              },
-              text: 'Some extremely cool feature',
-            },
-            'One-click 0$ <b>deploy</b> on GitHub Pages',
-            'Language and currency localization',
-            'Buttons with <b>haptic</b> feedback',
-            'Content pre-loading for high speed',
-            '<b>Low-code</b> approach to building onboardings',
-            'Many examples/presets',
-          ],
           button: {
             text: 'Website',
             url: 'https://ellemments.com/',
             target: '_self',
-          },
-        },
-        {
-          extends: 'default',
-          title: 'ellemments Website',
-          description: 'You are now viewing the ellemments website within the web app.',
-          button: {
-            text: 'Go Back',
-            onClick: () => {
-              // Logic to go back to the first slide
-              // You may need to consult the @tok/generation documentation for the specific syntax
-              // For example: Telegram.WebApp.close();
-            },
           },
         },
       ],
