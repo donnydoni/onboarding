55244121b8603e6cc1fad5c24559bf2084247294
donnydoni <doniyornishonov@gmail.com>
2024-03-21 10:21:00 +0900

    onboarding

--- app/src/config.ts
+++ app/src/config.ts
@@ -14,10 +14,52 @@ export default defineConfig({
           pagination: 'count',
           title: 'Welcome to ellemments TOKYO!',
           description: "ellemments",
+          list: [
+            {
+              media: {
+                type: 'icon',
+                src: import('./assets/icons/guide.svg'),
+                size: 30,
+              },
+              text: 'Some cool feature',
+            },
+            {
+              media: {
+                type: 'icon',
+                src: import('./assets/icons/track.svg'),
+                size: 30,
+              },
+              text: 'Some very cool feature',
+            },
+            {
+              media: {
+                type: 'icon',
+                src: import('./assets/icons/time.svg'),
+                size: 30,
+              },
+              text: 'Some extremely cool feature',
+            },
+            'One-click 0$ <b>deploy</b> on GitHub Pages',
+            'Language and currency localization',
+            'Buttons with <b>haptic</b> feedback',
+            'Content pre-loading for high speed',
+            '<b>Low-code</b> approach to building onboardings',
+            'Many examples/presets',
+          ],
           button: {
-            text: 'Website',
-            url: 'https://ellemments.com/',
-            target: '_self',
+            text: 'View Website',
+            reply_markup: {
+              keyboard: [
+                [
+                  {
+                    text: 'Open Website',
+                    web_app: {
+                      url: 'https://ellemments.com/',
+                    },
+                  },
+                ],
+              ],
+            },
           },
         },
       ],
